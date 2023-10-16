aa7525d1378d72df304be410daea595f525f9ff2
Moritz Wunderwald <moritzwunderwald@yahoo.de>
2023-10-16 11:02:33 +0200

    removed unused code

--- RAW_TO_STREAMS/CRC_PRE/discoverInputFiles.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-//input files have the neming convention <subjectId>_cardresp.txt
-module.exports = directory => fs.readdirSync(directory)
-    .filter(o => fs.lstatSync(`${directory}/${o}`).isFile())
-    .filter(file => file.endsWith('.txt') && (file.includes('cardresp') || file.includes('CRC')));
--- RAW_TO_STREAMS/CRC_PRE/main.js
+++
@@ -1,2 +0,0 @@
-const processCrcData = require('./processCrcData');
-processCrcData();
