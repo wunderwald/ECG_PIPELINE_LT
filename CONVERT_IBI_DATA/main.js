dd9ddb653bf04ab2c5e0baee3de8a7eea96c1be2
Moritz Wunderwald <moritzwunderwald@yahoo.de>
2023-11-23 18:52:41 +0100

    filename parsing done

---
+++ CONVERT_IBI_DATA/main.js
@@ -0,0 +1,64 @@
+import * as fs from 'fs';
+
+const INPUT_DIR = './dashboardOutputData';
+
+// filename components
+const EXPERIMENTS = ['lt_free_interaction'];
+const GROUPS = ['IL'];
+const ECG_LABELS = {
+    ecg1: ['ECG1', 'EKG_1', 'ECG_1'],
+    ecg2: ['ECG2', 'EKG_2', 'ECG_2']
+};
+
+// filename parsing helpers
+const getExperiment = f => {
+    for(const e of EXPERIMENTS){
+        if(f.includes(`${e}`)) return e;
+    }
+    return null;
+};
+
+const getGroup = f => {
+    for(const g of GROUPS){
+        if(f.includes(`_${g}_`)) return g;
+    }
+    return null;
+};
+
+const getEcgLabel = f => !!ECG_LABELS.ecg1.find(label => f.includes(label)) 
+    ? 'ecg1' 
+    : (!!ECG_LABELS.ecg2.find(label => f.includes(label))
+        ? 'ecg2'
+        : null);
+
+const getChannel = f => f.includes('channel0') 
+    ? 'channel0' 
+    : (f.includes('channel1')
+        ? 'channel1' 
+        : (f.includes('channel2') 
+            ? 'channel2' 
+            : null));
+
+const getDyadId = f => {
+    const match = f.match(/_(\d{2})_/);
+    return match ? match[1].replaceAll('_', '') : null;
+};
+
+// read files & parse filenames
+const _inputFiles = fs.readdirSync(INPUT_DIR)
+    .filter(file => fs.lstatSync(`${INPUT_DIR}/${file}`).isFile())
+    .filter(file => file.endsWith('.json'))
+    .map(file => ({
+        file: file,
+        path: `${INPUT_DIR}/${file}`,
+        experiment: getExperiment(file),
+        ecgLabel: getEcgLabel(file),
+        group: getGroup(file),
+        channel: getChannel(file),
+        dyadId: getDyadId(file)
+    }))
+
+// filter filenames (invalid syntax), throw warnings
+const inputFiles = [];
+
+// make dyads
