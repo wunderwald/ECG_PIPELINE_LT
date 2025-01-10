cee9ee8ebe7cfaafa32d3b4c468e63cd713ac6eb
Moritz Wunderwald <moritzwunderwald@yahoo.de>
2025-01-10 08:44:13 +0100

    added structure

--- RAW_TO_STREAMS/JS/processLT.js
+++ RAW_TO_STREAMS/JS/processLT.js
@@ -1,6 +1,45 @@
 const filterByMarkers = require('./filterByMarkers');
 
-const ltSegments = [
+const ltSegments_adults = [
+    {
+        label: 'lt_madlibs',
+        start: '1_1',
+        end: '2_1',
+        minDuration_ms: 0,
+        maxDuration_ms: Number.POSITIVE_INFINITY,
+    },
+    {
+        label: 'lt_video_a',
+        start: '3_1',
+        end: '4_1',
+        minDuration_ms: 300000, // 5min
+        maxDuration_ms: 600000, // 10 min
+    },
+    {
+        label: 'lt_video_b',
+        start: '3_2',
+        end: '4_2',
+        minDuration_ms: 300000, // 5min
+        maxDuration_ms: 600000, // 10 min
+    },
+    {
+        label: 'lt_free_interaction',
+        start: '5_1',
+        end: '6_1',
+        minDuration_ms: 600000, // 10min
+        maxDuration_ms: 1200000, // 20 min
+    },
+    {
+        label: 'lt_castle_knights',
+        start: '7_1',
+        end: '8_1',
+        minDuration_ms: 0,
+        maxDuration_ms: Number.POSITIVE_INFINITY
+    },
+];
+
+//TODO fix these
+const ltSegments_children = [
     {
         label: 'lt_madlibs',
         start: '1_1',
